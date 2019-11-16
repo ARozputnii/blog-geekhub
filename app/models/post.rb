6c6a2b8a955b38800f579fe9732d83d2d4cf703a
pradoav <a.rozputnii@gmail.com>
2019-11-16 15:06:10 +0200

    add model Author add gem-Faker

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
 	validates :title, :content, presence: :true
 	validates :title,  length: { minimum: 6 }
 	validates :content, length: { minimum: 10 }
+
+	belongs_to :author
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_191752) do
+ActiveRecord::Schema.define(version: 2019_11_16_125339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "gender"
+    t.string "birthday"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
