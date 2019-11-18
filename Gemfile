c13bcbad0008383f6325429b6d3eb0dd54455bf7
pradoav <a.rozputnii@gmail.com>
2019-11-18 21:35:55 +0200

    add count to show

--- Gemfile
+++ Gemfile
@@ -55,3 +55,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
 gem 'faker', '~> 2.7'
+gem 'impressionist', '~> 1.6', '>= 1.6.1'
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,8 @@ class Post < ApplicationRecord
 	validates :title,  length: { minimum: 6 }
 	validates :content, length: { minimum: 10 }
 
-	belongs_to :author
+	belongs_to :author, counter_cache: true
 	has_many :comments, dependent: :destroy
   has_one_attached :image
+  is_impressionable
 end
