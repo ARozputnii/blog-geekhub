41bef5c12c3e20bb306a95373113b33bde87c760
pradoav <a.rozputnii@gmail.com>
2019-11-24 23:35:52 +0200

    edit comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,37 @@
 class CommentsController < ApplicationController
+
+  before_action :find_post
+  before_action :find_comment, only: [:show, :edit, :update]
+
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
 
   def show
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
   end
 
-  def destroy
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
-    @comment.destroy
-    redirect_to post_path(@post)
+  def edit
   end
 
+  def update
+    if @comment.update(comment_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
   private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+  def find_comment
+    @comment = @post.comments.find(params[:id])
+  end
   def comment_params
-    params.require(:comment).permit(:commenter, :body)
+    params.require(:comment).permit(:author_id, :body)
   end
 end
