bb5d35eb62d203e934bef8c714c679ce16fb29c3
pradoav <a.rozputnii@gmail.com>
2019-12-02 08:30:39 +0200

    annotate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  author_id  :bigint
+#
+# Indexes
+#
+#  index_posts_on_author_id  (author_id)
+#
+
 class Post < ApplicationRecord
 
 	validates :author_id, presence: true
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  ancestry   :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  author_id  :bigint
+#  post_id    :bigint           not null
+#
+# Indexes
+#
+#  index_comments_on_ancestry   (ancestry)
+#  index_comments_on_author_id  (author_id)
+#  index_comments_on_post_id    (post_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (post_id => posts.id)
+#
+
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
