72c30e084b1b88a03bc98210a6e6e5551b0f752d
pradoav <a.rozputnii@gmail.com>
2019-12-06 00:43:25 +0200

    add bootstrap js

--- app/models/author.rb
+++ app/models/author.rb
@@ -37,7 +37,7 @@ class Author < ApplicationRecord
             format:     { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
 
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_digest, presence: true, length: { minimum: 8 }
 
 
 end
