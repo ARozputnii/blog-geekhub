a4e6109867cc131f4bd009b059f4773e62b9c974
pradoav <a.rozputnii@gmail.com>
2019-12-16 15:02:57 +0200

    refactoring to heroku

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,8 +1,7 @@
 ActiveAdmin.register_page "Dashboard" do
 
   controller do
-    http_basic_authenticate_with :name =>  Rails.application.credentials.aws[:activadmin] ||  ENV['activadmin'] ,
-                                 :password => Rails.application.credentials.aws[:activadmin] ||  ENV['activadmin']
+    http_basic_authenticate_with :name =>  Rails.application.credentials.aws[:activadmin], :password => Rails.application.credentials.aws[:activadmin]
   end
 
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
